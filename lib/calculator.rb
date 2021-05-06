655c724591bef7d13de81f5c6b2e918e170d4362
Lucky <vagyasri2011@gmail.com>
2021-05-06 21:46:25 +0200

    Create Calculator

---
+++ lib/calculator.rb
@@ -0,0 +1,14 @@
+class Calculator
+    def add(a, b, c)
+      a + b + c
+    end
+    def subtract(a, b)
+        a - b
+    end
+    def multiply(a, b)
+        a * b
+    end
+    def divide(a, b)
+        a / b
+    end
+end    
---
+++ spec/calculator_spec.rb
@@ -0,0 +1,28 @@
+require './lib/calculator'
+
+describe Calculator do
+    describe "#add" do
+      it "returns the sum of two numbers" do
+        calculator = Calculator.new
+        expect(calculator.add(2, 5, 7)).to eql(14)
+      end
+    end  
+    describe "#subtract" do
+      it "returns the difference of two numbers" do
+        calculator = Calculator.new
+        expect(calculator.subtract(5, 2)).to eql(3)
+      end
+    end
+    describe "#multiply" do
+        it "returns the multiplication of two numbers" do
+          calculator = Calculator.new
+          expect(calculator.multiply(5, 2)).to eql(10)
+        end
+    end
+    describe "#divide" do
+        it "returns the division of two numbers" do
+            calculator = Calculator.new
+            expect(calculator.divide(10, 5)).to eql(2)
+        end
+    end
+  end
