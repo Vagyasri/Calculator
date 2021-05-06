15d471b817b2a14d0016a8ba2b937022f0c54697
Lucky <vagyasri2011@gmail.com>
2021-05-06 22:05:01 +0200

    Fix rubocop

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,14 +1,23 @@
+# frozen_string_literal: true
+
+# rubocop: disable Naming/MethodParameterName, Style/Documentation
+
 class Calculator
-    def add(a, b, c)
-      a + b + c
-    end
-    def subtract(a, b)
-        a - b
-    end
-    def multiply(a, b)
-        a * b
-    end
-    def divide(a, b)
-        a / b
-    end
-end    
+  def add(a, b, c)
+    a + b + c
+  end
+
+  def subtract(a, b)
+    a - b
+  end
+
+  def multiply(a, b)
+    a * b
+  end
+
+  def divide(a, b)
+    a / b
+  end
+end
+
+# rubocop: enable Naming/MethodParameterName, Style/Documentation
