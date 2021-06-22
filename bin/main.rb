26ec7be4b30e7d6853365ae7764678dca4cbc47d
Lucky <vagyasri2011@gmail.com>
2021-06-22 21:38:19 +0200

    Remove linter error

--- bin/main.rb
+++ bin/main.rb
@@ -2,25 +2,25 @@
 # frozen_string_literal: true
 
 # This is a simple calculater of 2 operands
-  puts 'Enter 1st Number'
-  a = gets.chomp().to_f
-  puts 'Enter 2nd Number'
-  b = gets.chomp().to_f
-  puts 'Give the operator'
-  oper = gets.chomp()
-    case oper
-    when '+'
-      puts (a + b)
-    when '-'
-      puts (a - b)
-    when '*'
-      puts (a * b)
-    when '/'
-      puts (a / b)
-    when '%'
-      puts (a % b)  
-    when '**'
-      puts (a**b)
-    else
-      'Invalid Operator'
-    end
+puts 'Enter 1st Number'
+a = gets.chomp.to_f
+puts 'Enter 2nd Number'
+b = gets.chomp.to_f
+puts 'Give the operator'
+oper = gets.chomp
+case oper
+when '+'
+  puts(a + b)
+when '-'
+  puts(a - b)
+when '*'
+  puts(a * b)
+when '/'
+  puts(a / b)
+when '%'
+  puts(a % b)
+when '**'
+  puts(a**b)
+else
+  'Invalid Operator'
+end
